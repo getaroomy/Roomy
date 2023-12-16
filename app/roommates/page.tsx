5412974925b5fb3fde7e6db4811f13fb0d7cc989
Matthew Gaim <matthewgaim@gmail.com>
2023-12-15 18:04:27 -0800

    Show if there are no roommate matches

--- app/roommates/page.tsx
+++ app/roommates/page.tsx
@@ -5,6 +5,7 @@ import { useSearchParams, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import {UserAuth} from "@/app/context/AuthContext";
 import { getAvailableRoommates } from "@/app/actions";
+import Link from "next/link";
 
 export default function Roommates() {
   const searchParams = useSearchParams();
@@ -13,13 +14,14 @@ export default function Roommates() {
   const {user, loading} = UserAuth();
 
   const [roommates,setRoommates] = useState<Array<RoommatePreviewDetails> | null>();
-
+  const [loadingRoommates, setLoadingRoommates] = useState(true);
   useEffect(()=>{
 		if (!user && !loading) router.push("/auth");
 		if (user){
       const getRoommates = async () => {
         const res = await getAvailableRoommates(user, page);
         setRoommates(res);
+        setLoadingRoommates(false);
       }
       getRoommates();
 		}
@@ -30,7 +32,7 @@ export default function Roommates() {
     <main className="py-12 px-8">
       <h1 className="flex justify-center text-2xl pb-4 font-bold italic">Find Roomies</h1>
       <div className="flex flex-wrap justify-center gap-4">
-        {roommates && roommates.map((person: RoommatePreviewDetails, index: number)=>{
+        {roommates ? roommates.map((person: RoommatePreviewDetails, index: number)=>{
           return (
               <RoommateCard
                 key={index}
@@ -40,7 +42,12 @@ export default function Roommates() {
                 photoURL={person.photoURL}
                 city={person.city} />
           );
-        })}
+        }) : !loadingRoommates &&
+        <div className="text-center">
+            <h1>No Roomies found :( </h1>
+            <h2>Check if your <Link className="text-purple-600" href={`/profile/${user?.uid}/edit`}>profile</Link> is updated</h2>
+        </div>
+        }
       </div>
     </main>
   )
