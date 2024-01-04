e7d66377cd3fb069d85380b9139c95e8498916c1
Matthew Gaim <matthewgaim@gmail.com>
2024-01-03 22:45:26 -0800

    UI for creating a feed post

---
+++ app/components/createFeedPost.tsx
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from 'react';
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+} from "@/components/ui/carousel";
+import Image from 'next/image';
+
+export default function CreateFeedPost({createNewPost}: {createNewPost: (text: string, files?: Array<File>) => void}) {
+    const [postText, setPostText] = useState<string>("");
+    const [media, setMedia] = useState([]);
+    const [images, setImages] = useState([] as any);
+
+    useEffect(() => {
+        if (images.length < 1) return;
+        const newImageUrls: any = [];
+        images.forEach((image:any) => newImageUrls.push(URL.createObjectURL(image)));
+        setMedia(newImageUrls);
+      }, [images]);
+
+    function onImageChange(e: any) {
+        setImages([...e.target.files]);
+    }
+
+    const createNewFeedPost = () => {
+        createNewPost(postText, images);
+    }
+
+    return (
+        <div className='w-full md:w-1/2 lg:w-1/2 bg-white shadow-md p-4'>
+            <h1 className='text-lg font-bold'>Create a Post</h1>
+            <textarea
+                className='pt-1 w-full outline-none'
+                placeholder='Start yappin..'
+                value={postText}
+                onChange={(e)=>setPostText(e.currentTarget.value)}
+            />
+            {media?.length !== 0 &&
+                <Carousel className="w-full">
+                    <CarouselContent className="-ml-1 mr-1">
+                        {media.map((mediaUrl, index) => (
+                            <CarouselItem key={index} className="pl-1 md:basis-1/2 lg:basis-1/3">
+                                <div className="p-1">
+                                    <Image
+                                        src={mediaUrl}
+                                        width={512} height={512}
+                                        alt={"Users Picture"} />
+                                </div>
+                            </CarouselItem>
+                        ))}
+                    </CarouselContent>
+                    <CarouselPrevious className='bg-white' />
+                    <CarouselNext className='bg-white' />
+                </Carousel>
+            }
+            <div className=''>
+                <input type="file" multiple accept="image/*" className='w-fit' onChange={onImageChange} />
+                <button onClick={createNewFeedPost} className='bg-purple-200 hover:bg-purple-500 rounded-xl py-2 px-4 float-right'>Post</button>
+            </div>
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,28 +2,42 @@
 import { useEffect, useState } from "react"
 import { UserAuth } from "./context/AuthContext"
 import { useRouter } from "next/navigation";
+import CreateFeedPost from "@/app/components/createFeedPost";
 import FeedPost from "@/app/components/feedPost";
-import { FeedPostDetails } from "./lib/exports";
+import { FeedPostDetails } from "@/app/lib/exports";
+import { createNewFeedPost, getFeedPosts } from "./actions";
 
 export default function Home() {
   const router = useRouter();
   const {user, loading} = UserAuth();
   const [feedPosts, setFeedPosts] = useState<Array<FeedPostDetails>>();
 
+  const createNewPost = (text: string, files: any) => {
+    if (user) createNewFeedPost(user, text, files);
+  }
+
   useEffect(()=>{
     if (!user && !loading) router.push("/auth");
+    if (user) {
+      const getFeedPostsFromServer = async () => {
+        const res = await getFeedPosts(user);
+        setFeedPosts(res);
+      }
+      getFeedPostsFromServer();
+    }
   },[loading, router, user]);
   return (
     <main className="flex flex-col items-center justify-between p-24 space-y-4">
-      <h1>You&rsquo;re on: app/page</h1>
+      <CreateFeedPost createNewPost={createNewPost} />
       {feedPosts?.length && feedPosts.map((post, index)=>{
         return (
           <FeedPost key={index}
             uid={post.uid}
-            profilePic={post.profilePic}
+            photoURL={post.photoURL}
             displayName={post.displayName}
             city={post.city}
             post_text={post.post_text}
+            media={post.media}
             date={post.date} />
         );
       })}
